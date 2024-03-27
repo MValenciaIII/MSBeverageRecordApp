9bded2d79887f16bd358baef6327d3d8ecbde82a
ZachDubroc <zvd1993@gmail.com>
2024-03-27 14:57:29 -0500

    csv

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,26 +25,18 @@ using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Drawing;
 using System.Drawing.Printing;
-
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
 using System.Data;
 using System.Reflection;
-
-
+using System.Xml.Linq;
 //TODO
-// export to csv on button click
-
-
-
-
 
 namespace MSBeverageRecordApp {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
-
 
         //GLOBAL VARIABLE
         string file = "";
@@ -53,7 +45,6 @@ namespace MSBeverageRecordApp {
         public class urlResult {
             public string[] results { get; set; }
         }
-
         public MainWindow() {
 
             InitializeComponent();
@@ -67,34 +58,31 @@ namespace MSBeverageRecordApp {
                new MediaTypeWithQualityHeaderValue("application/json"));
             //THIS IS VARIABLE TO GET OBJECT DATA FROM API
             var response = client.GetAsync(client.BaseAddress).Result;
+
             //IF THE RESPONSE VARIABLE IS TRUE RUN THIS CODE.
             if (response.IsSuccessStatusCode) {
-
-
                 ////CONVERTING OBJECT "response" variable DATA TO STRING 
                 var dataobjects = response.Content.ReadAsStringAsync().Result;
                 //CURRENTLY TRYING TO CHANGE OUR STRING TO OBJECT DATA VVV
                 RootObject deserializeObject = new RootObject();
                 deserializeObject.Items = JsonSerializer.Deserialize<List<Records>>(dataobjects);
-
                 //How to set the query data to the DATAGRID element.
                 MSBeverageRecordApp.ItemsSource = deserializeObject.Items;
 
+                #region CSV
+                //vars to hold rows, cost, headers, col count
                 StringBuilder sb = new StringBuilder();
-
-
                 decimal totalCost = 0.0m;
-                string headerline = "id, category, company, model, serial, purchase date, cost, location, sub-location";
+                string headerline = "id, category, company, model, serial, purchase date, cost, location, sub-location, ";
                 string[] cols = headerline.Split(',');
                 colCount = cols.Length;
                 rows = new string[deserializeObject.Items.Count + 3];
+                //set headers as first row
                 rows[0] = headerline + "\n";
                 //loop over list and set values of each row into an array
                 for (int i = 1; i <= deserializeObject.Items.Count; i++) {
                     //clear string on each iteration
                     sb.Clear();
-                    //set headers as first row
-
                     //add values
                     sb.Append(deserializeObject.Items[i - 1].record_id.ToString() + ",");
                     sb.Append(deserializeObject.Items[i - 1].categoryName.ToString() + ",");
@@ -107,54 +95,57 @@ namespace MSBeverageRecordApp {
                     sb.Append(deserializeObject.Items[i - 1].sub_location.ToString() + "\n");
                     //get total cost
                     totalCost += (decimal)deserializeObject.Items[i - 1].cost;
-
-                    //assign current string value to be one row
-
                     //remove trailing comma on last row
                     if (i == deserializeObject.Items.Count) {
-                        sb.Remove(sb.Length-2,1);    
+                        sb.Remove(sb.Length - 2, 1);
                     }
+                    //assign current string value to be one row
                     rows[i] = sb.ToString();
-                    //test output
                 }
+
                 consoleOutput.Text = $"{totalCost:C}";
                 sb.Clear();
-                 
                 sb.Append("\ntotal equipment cost: " + totalCost.ToString());
-                
-                rows[deserializeObject.Items.Count +1] = sb.ToString();  
-
+                rows[deserializeObject.Items.Count + 1] = sb.ToString();
             }//end if statusOK
-
         }//end main
 
         public void Saving(string filePath, string[] array, int num) {
+
             //VARIABLE
             int count = 0;
-
             //loop over rows and append lines
             for (int i = 0; i < array.Length; i++) {
                 count++;
                 System.IO.File.AppendAllText(file, array[i]);
                 if (count > num) {
+                    //start new line after printing each cell in a row
                     System.IO.File.AppendAllText(file, "\n");
                 }
+                
             }//end for
-
         }//end function
-
         private void muiSave_Click(object sender, RoutedEventArgs e) {
             //create a save file dialog object
             SaveFileDialog sfdSave = new SaveFileDialog();
-            //open th edialog and wait for the user to make a selection
+            //open the dialog and wait for the user to make a selection
+            sfdSave.DefaultExt = "csv";
+            sfdSave.Filter =
+                "Text files (*.csv)|*.txt|All files (*.*)|*.*";
             bool fileSelected = (bool)sfdSave.ShowDialog();
             file = sfdSave.FileName;
             if (fileSelected == true) {
-
                 Saving(file, rows, colCount);
-
+                
             }//end if
         }
+        #endregion
+
+
+
+
+
+
 
         //THIS IS WHAT IS GOING TO BE THE ITEM SOURCE for the DATAGRID
         //THIS IS SETTING UP A PLACE TO STORE EACH OBJECT ATTRIBUTE INSIDE A LIST 
@@ -175,36 +166,11 @@ namespace MSBeverageRecordApp {
 
             public List<Records> Items { get; set; }
         }//end class
-
-
         private void addRecord(object sender, RoutedEventArgs e) {
             //MSBeverageRecordApp.Visibility = Visibility.Hidden;
             CreateRecord window = new CreateRecord();
-
             window.Show();
+            //waldo
         }//end function
-         //waldo
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        static async Task TestAPI(HttpClient client) {
-
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e) {
-
-        }
-
     }//end class
 }//end namespace
