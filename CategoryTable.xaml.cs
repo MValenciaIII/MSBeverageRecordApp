98bb0fcd460ef4d7e42c52842640344da7cceb8d
Darian Nelson <darian.nelson@mscoding.org>
2024-04-04 14:57:35 -0500

    Updating Category Page and outlining to do

--- CategoryTable.xaml.cs
+++ CategoryTable.xaml.cs
@@ -13,17 +13,22 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MSBeverageRecordApp
-{
+namespace MSBeverageRecordApp {
     /// <summary>
     /// Interaction logic for CategoryTable.xaml
     /// </summary>
-    public partial class CategoryTable : Page
-    {
-        public CategoryTable()
-        {
+    public partial class CategoryTable : Page {
+        public CategoryTable() {
             InitializeComponent();
+            //TO DO & NOTES:
+            //USE REQ.PARAMS TO MAKE A POST REQUEST TO ADD A CATEGORY TO MYSQL DATABASE
+            //CATEGORY TABLE ALREADY AUTO INCREMENTS, NO NEED TO ADD ID
+
+            //IDEAS:
+            //USE A HTTP CLIENT/REQ.PARAMS
+
         }//end main CategoryTable
 
     }//end class
+
 }//end namespace
