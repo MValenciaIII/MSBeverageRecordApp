00b6cc108e2d1b18c37d5b2c49ca34374e891cac
ZachDubroc <zvd1993@gmail.com>
2024-04-15 12:19:03 -0500

    Update CrudWindow.xaml.cs

--- CrudWindow.xaml.cs
+++ CrudWindow.xaml.cs
@@ -96,7 +96,8 @@ namespace MSBeverageRecordApp
                 deserializeObject.Items = JsonSerializer.Deserialize<List<Records>>(dataobjects);
                 //How to set the query data to the DATAGRID element.
 
-                //DateFormat();
+                DateFormat();
+
                 MSBeverageRecordGrid.ItemsSource = deserializeObject.Items;
 
                 //tables to pull id's/foreign keys
@@ -146,25 +147,28 @@ namespace MSBeverageRecordApp
 
         //TODO add print button/function, fix print format
 
-        //public void DateFormat() {
-        //    //DateTime n = new DateTime();
-        //    //DateTimeOffset fDate = new DateTimeOffset(n);
+        public void DateFormat() {
+            DateTime n = new DateTime();
+            DateTimeOffset fDate = new DateTimeOffset(n);
 
-        //    //to reset^ use that line in loop and save a separate edit
+            //to reset^ use that line in loop and save a separate edit
 
-        //    //2024-03-25T05:00:00.000Z
-        //    //format all values from ^
+            //2024-03-25T05:00:00.000Z
+            //format values from ^
+            //only format if length of str is less than 11
 
-        //    for (int i = 0; i < deserializeObject.Items.Count; i++) {
-        //        //deserializeObject.Items[i].purchase_date = "2024-03-25T05:00:00.000Z";
-        //        //date -> dd/mm/yyyy
-        //        //string dateString = deserializeObject.Items[i].purchase_date;
-        //        //DateTimeOffset offsetDate;
-        //        // String with date only
-        //        //offsetDate = DateTimeOffset.Parse(dateString);
-        //        //deserializeObject.Items[i].purchase_date = offsetDate.ToString("d");
-        //    }
-        //}
+            for (int i = 0; i < deserializeObject.Items.Count; i++) {
+                //deserializeObject.Items[i].purchase_date = "2024-03-25T05:00:00.000Z";
+                if (deserializeObject.Items[i].purchase_date.Length > 11) {
+                    //date -> dd/mm/yyyy
+                    string dateString = deserializeObject.Items[i].purchase_date;
+                    DateTimeOffset offsetDate;
+                    // String with date only
+                    offsetDate = DateTimeOffset.Parse(dateString);
+                    deserializeObject.Items[i].purchase_date = offsetDate.ToString("d");
+                }
+            }//end for
+        }
 
         public void Saving(string filePath, string[] array, int num)
         {
@@ -330,7 +334,6 @@ namespace MSBeverageRecordApp
             {
                 var responseContent = response.Content.ReadAsStringAsync().Result;
                 var postResponse = JsonSerializer.Deserialize<Records>(responseContent);
-
 
             }
             else
